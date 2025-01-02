f4cd120676570bcee8c08c84c4400da44761f726
YashMengji <yashmengji2172005@gmail.com>
2025-01-02 23:59:28 +0530

    comments logic updated

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 // import { getPosts } from '../services/posts'
-import {Link} from "react-router-dom"
-import { useAsync } from '../hooks/useAsync'
+import { Link } from "react-router-dom"
+import { useAsync, useAsyncFn } from '../hooks/useAsync'
+import { createComment } from "../services/comments";
+import CommentForm from "./CommentForm";
+import {usePost} from "../contexts/PostContext"
+import CommentList from "./CommentList";
 
 function Posts() {
   // const {loading, error, value: posts} = useAsync(getPosts)
+
+  const [showComments, setShowComments] = useState(false)
 
   const posts = [
     {
@@ -15,6 +21,13 @@ function Posts() {
     }
   ]
 
+  const {post, rootComments, createLocalComment} = usePost();
+  const {loading, error, execute: createCommentFn} = useAsyncFn(createComment);
+
+  function onCommentCreate(message) {
+    return createCommentFn({postId: post.id, message}).then( createLocalComment )
+  }
+
   // if(loading) return <h1>Loading</h1>
   // if(error) return <h1 className='error-msg'>{error}</h1>
 
@@ -22,41 +35,61 @@ function Posts() {
     <div className="posts-div">
       {
         posts.map((post) => {
-                  return(
-                    <div className="div-post" key={post._id}>
-                      <div className="div-poster-info">
-                        <div className="div-poster-img">
-                          <img className='poster-img' src="/images/defaultProfileImg.png" alt="Avatar"  />
-                        </div>
-                        <div className="div-poster-username">
-                          YashMengji
-                        </div>
-                      </div>
-                      <div className="div-post-title">
-                        {post.title}
-                      </div>
-                      <div className="div-post-body">
-                        {post.body}
-                      </div>
-                      <div className="div-post-img">
-                        <img src="/images/image.png" alt="" className="post-img" />
-                      </div>
-                      <div className="div-post-activity-bar">
-                        <div className="div-post-like">
-                          <i className="fa-regular fa-heart fa-xl"></i>
-                        </div>
-                        <div className="div-post-comment">
-                          <i className="fa-regular fa-comment fa-xl "></i>
-                        </div>
-                      </div>
+          return (
+            <div className="div-post" key={post._id}>
+              <div className="div-poster-info">
+                <div className="div-poster-img">
+                  <img className='poster-img' src="/images/defaultProfileImg.png" alt="Avatar" />
+                </div>
+                <div className="div-poster-username">
+                  YashMengji
+                </div>
+              </div>
+              <div className="div-post-title">
+                {post.title}
+              </div>
+              <div className="div-post-body">
+                {post.body}
+              </div>
+              <div className="div-post-img">
+                <img src="/images/image.png" alt="" className="post-img" />
+              </div>
+              <div className="div-post-activity-bar">
+                <div className="div-post-like">
+                  <i className="fa-regular fa-heart fa-xl"></i>
+                </div>
+                <div className="div-post-comment">
+                  {
+                    !showComments ? (<i className="fa-regular fa-comment fa-xl " onClick={() => setShowComments(true)}></i>) :
+                      (<i className="fa-solid fa-comment fa-xl" onClick={() => setShowComments(false)}></i>)
+                  }
+                </div>
+              </div>
+              <div className="div-post-comment-section">
+                <div className="div-comment-section-heading">
+                  Comments
+                </div>
+                <div>
+                  <CommentForm
+                    loading={loading}
+                    error={error}
+                    onSubmit={onCommentCreate}
+                  />
+                  {rootComments != null && rootComments.length && (
+                    <div className="mt-4">
+                      <CommentList comments={rootComments} />
                     </div>
-                  
-                  )
-                })
+                  )}
+                </div>
+              </div>
+            </div>
+
+          )
+        })
       }
     </div>
-  )  
-    
+  )
+
 
 }
 
---
+++ contexts/PostContext.jsx
@@ -0,0 +1,88 @@
+import React, { createContext, useContext, useEffect, useMemo } from 'react'
+import { useParams } from "react-router-dom";
+import { useAsync } from "../hooks/useAsync";
+import { getOnePost } from "../services/posts";
+import { useState } from 'react';
+
+export const postContext = createContext();
+
+export function usePost(){
+  return useContext(postContext)
+}
+
+// Here childern is automatically passed when any <PostContext> <Post/> <PostContext /> 
+function PostContext({children}) {
+  
+  const {id} = useParams();
+  const {loading, error, value: post} =  useAsync(() => getOnePost(id), [id]);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    if(post?.comments == null) return undefined;
+    setComments(post.comments);
+  }, [post?.comments]);
+
+  useEffect(() => {
+    console.log(comments);
+  }, [comments])
+
+  const commentsByParentId = useMemo(() => {
+    if(comments == null) return []
+    const group = {};
+    comments.forEach(comment => {
+      group[comment.parentId] ||= [];
+      group[comment.parentId].push(comment);
+    });
+
+    return group;
+  }, [comments]);
+
+  function getReplies(parentId){
+    return commentsByParentId[parentId]
+  }
+
+  function createLocalComment(comment) {
+    setComments(prevComments => [comment, ...prevComments]);  
+  }
+  function updateLocalComment(commentId, message) {
+    setComments(prevComments => {
+      return prevComments.map(comment => {
+        if(commentId == comment._id){
+          return {...comment, message};
+        }
+        else{
+          return comment;
+        }
+      })
+    });  
+  }
+  function deleteLocalComment(commentId) {
+    console.log(comments);
+    setComments(prevComments => {
+      return prevComments.filter(comment => comment._id !== commentId); // Filter out the comment to delete
+    });
+  }
+  
+
+  return (
+    <postContext.Provider value={
+        {
+          post: {id, ...post}, //id is const defined above...doing this because on server side we haven't extracted _id from database and sent along with post object
+          rootComments: commentsByParentId[undefined],
+          getReplies,
+          createLocalComment,
+          updateLocalComment,
+          deleteLocalComment,
+        }                        
+      }
+    > 
+    {
+      loading ? (<h1>Loading...</h1>) :
+      error ? (<h1>Error</h1>) :
+      children
+    }
+    </postContext.Provider>
+  )
+}
+
+export default PostContext
